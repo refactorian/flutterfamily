b9f111d742d2a65141223b129d07add1ba157328
refactorian <refactorian@gmail.com>
2026-07-02 16:16:19 +0600

    feat: update project logo, add social card, and configure as default site image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/flutter-family-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
