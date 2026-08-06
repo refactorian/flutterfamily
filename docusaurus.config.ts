dbd0670e169b00567568ea85c75a70149af014d2
refactorian <refactorian@gmail.com>
2026-08-06 12:28:42 +0600

    feat: add and configure docusaurus sitemap plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,6 +71,13 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tests/**'],
+          filename: 'sitemap.xml',
+        },
       } satisfies Preset.Options,
     ],
   ],
